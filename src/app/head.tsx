2f187c065ec4bba2d025797527677935a3608506
unknown <alstn549@naver.com>
2025-05-30 10:59:38 +0900

    SEO setting

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="fs13U9M2qujjIHTs0bt5uP5nhdLx91m1zgn_8HC7ErA"
+      />
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,34 @@ import HeaderSelector from "@/components/Header/HeaderSelector";
 
 export const metadata: Metadata = {
   title: "Piction",
-  description: "당신만의 스토리를 만들어보세요",
+  description: "AI를 통해 당신만의 스토리를 만들어보세요",
   icons: {
     icon: "/favicon-32x32.png",
+  },
+  keywords: [
+    "스토리 생성",
+    "AI 스토리",
+    "창작",
+    "인터랙티브 이야기",
+    "Piction",
+    "AI소설",
+  ],
+  metadataBase: new URL("https://www.piction.site"),
+  openGraph: {
+    title: "Piction",
+    description: "당신만의 스토리를 만들어보세요",
+    url: "https://www.piction.site",
+    siteName: "Piction",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Piction 대표 이미지",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
   },
 };
 
